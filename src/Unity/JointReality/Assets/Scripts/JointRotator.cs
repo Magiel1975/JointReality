22d2971f59e2da3bcc6569b332696b7d8a3fcb17
magiel van gaalen <magiel.van.gaalen@celox-ict.com>
2018-06-16 13:44:50 +0200

    Moving Rotator Scriptss

---
+++ src/Unity/JointReality/Assets/Scripts/JointRotator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointRotator : JointSelectable
+{
+    public enum RotationAxis
+    {
+        X,
+        Y,
+        Z
+    }
+
+    //[SerializeField]
+    //private GameObject JointObject;
+    [SerializeField]
+    private GameObject ChildArmObject;
+    [SerializeField]
+    private RotationAxis Axis;
+    [SerializeField]
+    private int Speed;
+
+    public bool IsRotating { get; set; }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (IsRotating)
+        {
+            Rotate();
+        }
+    }
+
+    private void Rotate()
+    {
+        switch(Axis)
+        {
+            case RotationAxis.X:
+                ChildArmObject.transform.Rotate(Time.deltaTime * Speed, 0, 0);
+                break;
+            case RotationAxis.Y:
+                ChildArmObject.transform.Rotate(0, Time.deltaTime * Speed, 0);
+                break;
+            case RotationAxis.Z:
+                ChildArmObject.transform.Rotate(0, 0, Time.deltaTime * Speed);
+                break;
+        }
+    }
+
+    public override void DoSelect()
+    {
+        base.DoSelect();
+        IsRotating = !IsRotating;
+    }
+}
---
+++ src/Unity/JointReality/Assets/Scripts/JointSelectable.cs
@@ -0,0 +1,37 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointSelectable : MonoBehaviour, IInputClickHandler {
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+ 
+        DoSelect();
+    }
+
+    private void OnMouseUp()
+    {
+        DoSelect();
+    }
+
+    private void OnMouseDown()
+    {
+        DoSelect();
+    }
+
+    public virtual void DoSelect()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
